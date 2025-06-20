5f86ca45661d750a74e8f40a9426c938f38d5453
Sagar Das <151948032+sagardas02@users.noreply.github.com>
2025-06-20 12:23:05 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -3,7 +3,7 @@ def call(String dockerhubUser, String imageName, String imageTag){
                     'credentialsId':"dockerHubCred",
                     passwordVariable:"dockerHubPass",
                     usernameVariable:"dockerHubUser")]){
-                sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}" 
+                sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}" 
                 sh "docker image tag ${imageName}:${imageTag} ${dockerHubUser}/${imageName}:${imageTag}"
                 sh "docker push ${dockerHubUser}/${imageName}:${imageTag}"
                 }
